301f65ad01b777cd35dd95a0bd41dcad49e096c0
unknown <daniel2001.cieslak@wp.pl>
2025-03-25 21:00:35 +0100

    Stworzenie nowych ikon narzędzi

--- app/views/ToolBarView.py
+++ app/views/ToolBarView.py
@@ -6,20 +6,20 @@ from PySide6.QtCore import QSize
 class ToolBarView(QToolBar):
     def __init__(self, MainWindow):
         super().__init__("Tools", MainWindow)
-        self.setIconSize(QSize(32, 32))
+        self.setIconSize(QSize(48, 48))
         self.setupUI()
 
     def setupUI(self):
         self.actionCreateTable = QAction(QIcon("app\\icons\\table.png"), "Create table", self)
         self.addAction(self.actionCreateTable)
 
-        self.actionCreate_1_1_Rel = QAction(QIcon("app\\icons\\1_1.png"), "Create 1:1 relationship", self)
+        self.actionCreate_1_1_Rel = QAction(QIcon("app\\icons\\1_1_rel.png"), "Create 1:1 relationship", self)
         self.addAction(self.actionCreate_1_1_Rel)
 
-        self.actionCreate_1_n_Rel = QAction(QIcon("app\\icons\\1_n.png"), "Create 1:n relationship", self)
+        self.actionCreate_1_n_Rel = QAction(QIcon("app\\icons\\1_n_rel.png"), "Create 1:n relationship", self)
         self.addAction(self.actionCreate_1_n_Rel)
 
-        self.actionCreate_n_n_Rel = QAction(QIcon("app\\icons\\n_n.png"), "Create n:n relationship", self)
+        self.actionCreate_n_n_Rel = QAction(QIcon("app\\icons\\n_n_rel.png"), "Create n:n relationship", self)
         self.addAction(self.actionCreate_n_n_Rel)
 
         self.actionSaveDiagram = QAction(QIcon("app\\icons\\saveDiagram.png"), "Save diagram", self)
